fd3a0ec414da5199c363074a441d8d3317a09c59
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-02-03 12:30:36 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Microservices Authantication and Authorization with:
+  - Open Policy Agent(Authorization)
+  - Traefik(API Gateway)
+  - FastAPI(Authantication service)
+  - Docker(Docker Compose)
+
+  - Kubernetes will be supported soon
---
+++ auth-opa-middleware/app.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const express = require('express')
+const morgan = require('morgan')
+const axios = require('axios')
+
+const app = express()
+const bodyParser = require('body-parser')
+const port = process.env.NODE_PORT || 3000
+
+const OPA_URL = 'http://opa:8181/v1/data/demo/opa'
+
+var user = { name: 'Jane', password: '456' }
+
+function indexHandler(req, res) {
+  var [username, password] = req.headers.authorization.split(':')
+  if (username === user.name && password === user.password) {
+    res.send('')
+  } else {
+    res.status(401).send('Unauthorized')
+  }
+    // axios.post(OPA_URL, 
+  //   { 
+  //     input: {
+  //       method: req.headers['x-forwarded-method'],
+  //       token: (req.headers['authorization'] || 'eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.e30.').replace(/bearer\s+/i, ''),
+  //       path: req.headers['x-forwarded-uri']
+  //     } 
+  //   },
+  //   { headers: { 'Content-Type': 'application/json' } })
+  // .then(function (resp) {
+  //   if ((resp.status === 200) && (resp.data.result.allow === true)) {
+  //     res.status(200).send('')
+  //   }
+  //   else {
+  //     res.status(403).send('Your are not authorized to perform this action')
+  //   }
+  // })
+  // .catch(function (err) {
+  //   res.status(500).json({ error: `Error in evaluating request with OPA: ${err.message}`})
+  // })
+}
+
+app.use(bodyParser.json())
+app.use(morgan('combined'))
+
+// We only get GET request from Traefik authForward middleware
+app.get('/', indexHandler)
+app.listen(port, () => console.log(`Middleware Service listening on port ${port}!`))
